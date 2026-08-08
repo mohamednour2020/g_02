5487d36ffd6c0605df6862067f050423be054cc7
mohamednour <mohamed.nour@xyrisdegital.com>
2026-08-08 14:24:07 +0300

    session 2 -flutter

--- lib/main.dart
+++ lib/main.dart
@@ -40,90 +40,162 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.teal,
-        toolbarHeight: 80,
-        title: Column(mainAxisAlignment: .spaceBetween,
-          children: [
-            Text("Hello World",style: TextStyle(color: Colors.white,fontSize: 26),),
-            Row(
-              mainAxisAlignment: .center,
-              children: [
-                Icon(Icons.person,color: Colors.white,),
-                Icon(Icons.person,color: Colors.white,),
-                Icon(Icons.person,color: Colors.white,),
-              ],
-            ),
-          ],
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          "Food App",
+          style: TextStyle(color: Colors.white, fontSize: 26),
         ),
-        centerTitle: true,
-        leading:Icon(Icons.person,color: Colors.white,),
+        centerTitle: false,
         actions: [
-          Icon(Icons.search,color: Colors.white,),
-          SizedBox(width: 8,),
-          Icon(Icons.settings,color: Colors.white,),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.notifications_none, color: Colors.white),
+              ),
+              Positioned(
+                right: 8,
+                top: 8,
+                child: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "3",
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
-
       ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: .center,
-          mainAxisAlignment: .start,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: ListView(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Material(
+              elevation: 1,
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.local_convenience_store_rounded,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Column(
+                      mainAxisAlignment: .center,
+                      crossAxisAlignment: .start,
+                      children: [
+                        Text(
+                          "Deliciuos Resturant",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.star_rounded, color: Colors.amber),
+                            SizedBox(width: 4),
+                            Text(
+                              "4.8",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              "(230 reviews)",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.shopping_bag_outlined,
+                              color: Colors.deepPurple,
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              "(+150 Orders)",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            SizedBox(
+              height: 120,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.blueAccent[100],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: .spaceEvenly,
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          child: Icon(Icons.emoji_food_beverage),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[100],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        Text(
+                          "Pizza",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: 5,
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(width: 4);
+                },
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
